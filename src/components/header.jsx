fcd7ea7cc60c1fbe9c8b7c4d8de47ebc9307a5a3
Mariano Camacho <macadema@gmail.com>
2023-05-08 22:05:40 -0300

    agregando rutas y componente Login con tailwind. Cambiando nombre de componente header a Header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,101 +1,129 @@
-import React from "react"
-import cart from "../assets/cart.svg"
-import favourite from "../assets/favourite.svg"
-import store from "../assets/store.svg"
-import user from "../assets/user.svg"
-import logo from "../assets/logo.png"
-import avatar from "../assets/avatar.png"
-import search from "../assets/search.svg"
-import chdown from "../assets/chevron-down.svg"
-import line from "../assets/line.svg"
+import React from "react";
+import cart from "../assets/cart.svg";
+import favourite from "../assets/favourite.svg";
+import store from "../assets/store.svg";
+import user from "../assets/user.svg";
+import logo from "../assets/logo.png";
+import avatar from "../assets/avatar.png";
+import search from "../assets/search.svg";
+import chdown from "../assets/chevron-down.svg";
+import line from "../assets/line.svg";
 
-import "../components/header.css"
+import "../components/header.css";
 
-function header() {
-
-
-    return (
-        <>
-            <header>
-                <div className="logo"><img src={logo} /></div>
-                <div className="group">
-                    <button className="menuButton"><img src={user} /></button>
-                    <div className="popover">
-                        <button>User Name<img src={chdown} /></button>
-                        <div className="menu">
-                            <button>
-                                <img id="user-avatar" src={avatar} />
-                                <span id="user-profile">Hola, User<span id="user-mail">user mail</span></span>
-                            </button>
-                            <button>Mi perfil</button>
-                            <img src={line} />
-                            <button>Notificaciones</button>
-                            <img src={line} />
-                            <button>Vender</button>
-                            <img src={line} />
-                            <button>Publicaciones activas</button>
-                            <img src={line} />
-                            <button>Historial</button>
-                            <img src={line} />
-                            <button>Billetera</button>
-                            <img src={line} />
-                            <button>Salir</button>
-                        </div>
-                    </div>
-                    <button className="menuButton"><img src={cart} /></button>
-                    <button className="menuButton"><img src={favourite} /></button>
-                    <button className="menuButton"><img src={store} /></button>
-                </div>
-                <nav>
-                    <div className="popover">
-                        <button><h2>TIENDA</h2><img src={chdown} /></button>
-                        <div className="menu-tienda">
-                            <div>
-                                <h2>Ver todo</h2>
-                                <h2>Nuevos ingresos</h2>
-                            </div>
-                            <div>
-                                <h2>Mujer</h2>
-                                <a>Abrigos/Camperas</a>
-                                <a>Accesorios</a>
-                                <a>Camisas</a>
-                                <a>Calzado</a>
-                                <a>Pantalones</a>
-                                <a>Polleras</a>
-                                <a>Remeras/Buzos</a>
-                                <a>Vestidos</a>
-                                <a>Otros</a>
-                            </div>
-                            <div>
-                                <h2>Hombre</h2>
-                                <a>Abrigos/Camperas</a>
-                                <a>Accesorios</a>
-                                <a>Camisas</a>
-                                <a>Calzado</a>
-                                <a>Pantalones</a>
-                                <a>Remeras/Buzos</a>
-                                <a>Otros</a>
-                            </div>
-                            <div>
-                                <h2>Accesorios</h2>
-                                <a>Lentes</a>
-                                <a>Mochilas</a>
-                                <a>Gorros</a>
-                                <a>Otros</a>
-                            </div>
-                        </div>
-                    </div>
-                    <button><h2>COMO FUNCIONA</h2></button>
-                    <button><h2>SOBRE NOSOTROS</h2></button>
-                    <button><h2>BLOG</h2></button>
-                    <form class="search-container">
-                        <input type="text" id="search-bar" placeholder="Buscar prendas, accesorios y mas..." />
-                        <button className="search-button"><img src={search} /></button>
-                    </form>
-                </nav>
-            </header>
-        </>
-    )
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="logo">
+          <img src={logo} />
+        </div>
+        <div className="group">
+          <button className="menuButton">
+            <img src={user} />
+          </button>
+          <div className="popover">
+            <button>
+              User Name
+              <img src={chdown} />
+            </button>
+            <div className="menu">
+              <button>
+                <img id="user-avatar" src={avatar} />
+                <span id="user-profile">
+                  Hola, User<span id="user-mail">user mail</span>
+                </span>
+              </button>
+              <button>Mi perfil</button>
+              <img src={line} />
+              <button>Notificaciones</button>
+              <img src={line} />
+              <button>Vender</button>
+              <img src={line} />
+              <button>Publicaciones activas</button>
+              <img src={line} />
+              <button>Historial</button>
+              <img src={line} />
+              <button>Billetera</button>
+              <img src={line} />
+              <button>Salir</button>
+            </div>
+          </div>
+          <button className="menuButton">
+            <img src={cart} />
+          </button>
+          <button className="menuButton">
+            <img src={favourite} />
+          </button>
+          <button className="menuButton">
+            <img src={store} />
+          </button>
+        </div>
+        <nav>
+          <div className="popover">
+            <button>
+              <h2>TIENDA</h2>
+              <img src={chdown} />
+            </button>
+            <div className="menu-tienda">
+              <div>
+                <h2>Ver todo</h2>
+                <h2>Nuevos ingresos</h2>
+              </div>
+              <div>
+                <h2>Mujer</h2>
+                <a>Abrigos/Camperas</a>
+                <a>Accesorios</a>
+                <a>Camisas</a>
+                <a>Calzado</a>
+                <a>Pantalones</a>
+                <a>Polleras</a>
+                <a>Remeras/Buzos</a>
+                <a>Vestidos</a>
+                <a>Otros</a>
+              </div>
+              <div>
+                <h2>Hombre</h2>
+                <a>Abrigos/Camperas</a>
+                <a>Accesorios</a>
+                <a>Camisas</a>
+                <a>Calzado</a>
+                <a>Pantalones</a>
+                <a>Remeras/Buzos</a>
+                <a>Otros</a>
+              </div>
+              <div>
+                <h2>Accesorios</h2>
+                <a>Lentes</a>
+                <a>Mochilas</a>
+                <a>Gorros</a>
+                <a>Otros</a>
+              </div>
+            </div>
+          </div>
+          <button>
+            <h2>COMO FUNCIONA</h2>
+          </button>
+          <button>
+            <h2>SOBRE NOSOTROS</h2>
+          </button>
+          <button>
+            <h2>BLOG</h2>
+          </button>
+          <form class="search-container">
+            <input
+              type="text"
+              id="search-bar"
+              placeholder="Buscar prendas, accesorios y mas..."
+            />
+            <button className="search-button">
+              <img src={search} />
+            </button>
+          </form>
+        </nav>
+      </header>
+    </>
+  );
 }
 
-export default header
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Login from "./components/Login.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  { name: "Home", path: "/", element: <App /> },
+  { name: "Login", path: "login", element: <Login /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
