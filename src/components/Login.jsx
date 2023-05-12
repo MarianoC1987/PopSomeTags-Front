603fdbb683bb3c8b2543e6fe5a7cdc95fddaf20b
Mariano Camacho <macadema@gmail.com>
2023-05-11 22:13:51 -0300

    arreglando el login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,12 @@
 function Login() {
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 bg-white">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img
-          className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Your Company"
-        />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
+    <div className="flex min-h-full flex-col justify-center rounded border-black w-full px-6 py-12 h-100 lg:px-8 bg-white ">
+      <div className="sm:mx-auto flex justify-between sm:w-full sm:max-w-sm ">
+        <h2 className="mt-10 text-6 font-bold leading-9 tracking-tight text-gray-600 underline-offset-8 underline decoration-6">
+          Iniciar sesión
+        </h2>
+        <h2 className="mt-10 text-6 font-bold leading-9 tracking-tight text-gray-600 underline-offset-8 underline decoration-6">
+          Registrarse
         </h2>
       </div>
 
@@ -17,9 +15,9 @@ function Login() {
           <div>
             <label
               for="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
+              className="flex text-xs font-small leading-6 text-gray-400"
             >
-              Email address
+              Correo electrónico
             </label>
             <div className="mt-2">
               <input
@@ -34,21 +32,13 @@ function Login() {
           </div>
 
           <div>
-            <div className="flex items-center justify-between">
+            <div className="flex ">
               <label
                 for="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
+                className="flex text-xs font-small text-gray-400"
               >
-                Password
+                Contraseña
               </label>
-              <div className="text-sm">
-                <a
-                  href="#"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
             </div>
             <div className="mt-2">
               <input
@@ -60,6 +50,14 @@ function Login() {
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
+            <div className="text-sm">
+              <a
+                href="#"
+                className="flex text-xs font-small text-gray-400 hover:text-indigo-500"
+              >
+                ¿Has olvidado tu contraseña?
+              </a>
+            </div>
           </div>
 
           <div>
@@ -67,20 +65,10 @@ function Login() {
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sign in
+              Iniciar Sesión
             </button>
           </div>
         </form>
-
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Not a member?
-          <a
-            href="#"
-            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >
-            Start a 14 day free trial
-          </a>
-        </p>
       </div>
     </div>
   );
